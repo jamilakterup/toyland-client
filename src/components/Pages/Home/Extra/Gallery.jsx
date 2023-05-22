0ebc03771ce6c2922d3941d651f283b7356bb6f5
Md Jamil Akter <jamilakterup@gmail.com>
2023-05-22 13:10:31 +0600

    gallery

---
+++ src/components/Pages/Home/Extra/Gallery.jsx
@@ -0,0 +1,28 @@
+import img1 from '../../../../assets/g4.jpeg'
+import img2 from '../../../../assets/g2.jpg'
+import img3 from '../../../../assets/g3.jpg'
+import img4 from '../../../../assets/imp1.jpg'
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+
+
+const Gallery = () => {
+    return (
+        <section className='my-36 container mx-auto'>
+            <h1 className='text-5xl font-bold text-[#0B2F20] text-center mb-20'>Most Popular Toys</h1>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide><img className='h-[300px]' src={img1} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='h-[300px]' src={img3} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='h-[300px]' src={img4} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='h-[300px]' src={img2} alt="" /></SwiperSlide>
+            </Swiper>
+        </section>
+    );
+};
+
+export default Gallery;
--- src/components/Pages/Home/Home/Home.jsx
+++ src/components/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Extra from "../Extra/Extra";
+import Gallery from "../Extra/Gallery";
 import Products from "../Products/Products";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Gallery />
             <Products />
             <Extra />
         </div>
