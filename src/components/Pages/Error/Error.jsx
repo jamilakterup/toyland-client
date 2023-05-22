7e8b051159cd9a236987949d76855ced3ffbbdf1
Md Jamil Akter <jamilakterup@gmail.com>
2023-05-22 18:40:00 +0600

    error status

--- src/components/Pages/Error/Error.jsx
+++ src/components/Pages/Error/Error.jsx
@@ -1,10 +1,12 @@
-import {Link} from 'react-router-dom';
-import error from '../../../assets/error.jpg'
+import {Link, useRouteError} from 'react-router-dom';
+import img from '../../../assets/error.jpg'
 const Error = () => {
+    const {error} = useRouteError();
     return (
         <div className='flex justify-center items-center h-screen bg-[#8BACC7]'>
-            <img className='w-[60em]' src={error} alt="" />
+            <img className='w-[60em]' src={img} alt="" />
             <Link className='btn bg-[#0B2F20] absolute top-[420px] right-[210px]' to='/'>Back To Home</Link>
+            <p className='text-xl bottom-[150px] absolute'>{error.message}</p>
         </div>
     );
 };
